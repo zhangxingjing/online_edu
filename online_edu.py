8f55f776823e0797d260ee5e0758348e779109e0
zchange <zhangxingjing@hotmail.com>
2017-07-25 09:00:49 -0700

    write your meaaage

--- online_edu.py
+++ online_edu.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import config
 
 app = Flask(__name__)
@@ -8,6 +8,12 @@ app.config.from_object(config)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/login/',methods=['GET','POST'])
+def login():
+    if request.method == 'get':
+        return render_template('login.html')
+    else:
+        return render_template('login.html' )
 
 if __name__ == '__main__':
     app.run()
